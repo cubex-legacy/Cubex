59b8321ec562e6b7a5d490d36aac44e0a821f3d1
Brooke Bryan <brooke@justdevelop.it>
2013-01-11 13:29:25 +0000

    Partials and Webpage

---
+++ src/Cubex/Core/Response/Webpage.php
@@ -0,0 +1,343 @@
+<?php
+/**
+ * @author  brooke.bryan
+ */
+namespace Cubex\Core\Response;
+
+use Cubex\Core\Http\DispatchableAccess;
+use Cubex\Core\Http\Request;
+use Cubex\Core\Http\Response;
+use Cubex\Events\Event;
+use Cubex\Events\EventManager as EM;
+use Cubex\Foundation\Renderable;
+use Cubex\Http\DispatchInjection;
+use Cubex\View\HtmlElement;
+use Cubex\View\Partial;
+use Cubex\View\ResponseAwareRenderable;
+
+class Webpage implements
+  ResponseAwareRenderable,
+  DispatchInjection,
+  DispatchableAccess
+{
+  /**
+   * @var \Cubex\Core\Http\Response
+   */
+  protected $_response;
+  /**
+   * @var \Cubex\Core\Http\Request
+   */
+  protected $_request;
+  protected $_title;
+  protected $_meta;
+  protected $_bodyAttributes;
+  protected $_renderables;
+
+  public function __construct()
+  {
+    $this->registerPageTitleListener();
+  }
+
+  public function __toString()
+  {
+    return $this->render();
+  }
+
+  /**
+   * @param \Cubex\Core\Http\Response $response
+   *
+   * @return $this
+   */
+  public function setResponse(Response $response)
+  {
+    $this->_response = $response;
+    return $this;
+  }
+
+  /**
+   * @param \Cubex\Core\Http\Request $request
+   *
+   * @return $this
+   */
+  public function setRequest(Request $request)
+  {
+    $this->_request = $request;
+    return $this;
+  }
+
+  /**
+   * @return \Cubex\Core\Http\Request
+   */
+  public function request()
+  {
+    return $this->_request;
+  }
+
+  /**
+   * @return \Cubex\Core\Http\Response
+   */
+  public function response()
+  {
+    return $this->_response;
+  }
+
+  public function setStatusCode($status = 200)
+  {
+    $this->_response->setStatusCode($status);
+    return $this;
+  }
+
+  /**
+   * Set Webpage Title
+   *
+   * @param $title
+   *
+   * @return WebPage
+   */
+  public function setTitle($title)
+  {
+    if($title instanceof Event)
+    {
+      $title = $title->getStr("title");
+      if($title !== null)
+      {
+        $this->_title = $title;
+      }
+    }
+    else
+    {
+      $this->_title = $title;
+    }
+
+    return $this;
+  }
+
+  public function getTitle()
+  {
+    return $this->_title;
+  }
+
+  public function registerPageTitleListener()
+  {
+    EM::listen(EM::CUBEX_PAGE_TITLE, array($this, "setTitle"));
+    return $this;
+  }
+
+  /**
+   * Page Character Set
+   *
+   * @return string
+   */
+  public function getCharset()
+  {
+    return 'UTF-8';
+  }
+
+  /**
+   * Get defined meta tags
+   *
+   * @param null $key
+   *
+   * @return mixed
+   */
+  public function getMeta($key = null)
+  {
+    if($key === null)
+    {
+      return $this->_meta;
+    }
+    else return $this->_meta[$key];
+  }
+
+  /**
+   * Build MetaTags
+   *
+   * @return string
+   */
+  public function getMetaHTML()
+  {
+    if(!$this->_meta) return '';
+    $html = '';
+    foreach($this->_meta as $name => $content)
+    {
+      $html .= '<meta name="' . $name . '" content="' . $content . '" />';
+    }
+
+    return $html;
+  }
+
+  /**
+   * Get CSS
+   *
+   * @return string
+   */
+  public function getHead()
+  {
+    $cssHeaders = new Partial(
+      '<link type="text/css" rel="stylesheet" href="%s" />'
+    );
+    /*$cssUris    = Prop::getResourceUris('css');
+    if($cssUris)
+    {
+      $cssHeaders->addElements($cssUris);
+    }*/
+    return $cssHeaders . $this->getMetaHTML();
+  }
+
+  /**
+   * Render body content or captured content
+   *
+   * @return mixed
+   */
+  public function getBody()
+  {
+    $result = '';
+    foreach($this->_renderables as $render)
+    {
+      if($render instanceof Renderable)
+      {
+        $result .= $render->render();
+      }
+    }
+    return $result;
+  }
+
+  /**
+   * Include JavaScript
+   *
+   * @return \Cubex\View\Partial
+   */
+  public function getClosing()
+  {
+    $jsItems = new Partial(
+      '<script type="text/javascript" src' . '="%s"></script>'
+    );
+    /*$jsUris  = Prop::getResourceUris('js');
+    if($jsUris)
+    {
+      $jsItems->addElements($jsUris);
+    }*/
+    return $jsItems;
+  }
+
+  /**
+   * Build HTML upto opening Body tag
+   *
+   * @return string
+   */
+  public function renderHead()
+  {
+    $charset = $this->getCharset();
+    $title   = $this->getTitle();
+    $head    = $this->getHead();
+
+    $method = \strtoupper($this->request()->requestMethod());
+
+    $requestUrl = $this->request()->path();
+    $requestUrl .= '?' . \http_build_query(
+      $this->request()->getVariables(), '', '&amp;'
+    );
+
+    $noscript = '<meta http-equiv="refresh" content="0; URL=';
+    $noscript .= $requestUrl . '&amp;__noscript__=1" />';
+    if($this->request()->jsSupport() === false) $noscript = '';
+
+    $response = "<!DOCTYPE html>\n"
+    . '<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->'
+    . '<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->'
+    . '<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->'
+    . '<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->'
+    . "\n"
+    . '<head><meta charset="' . $charset . '" />'
+    . '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">'
+    . '<meta name="viewport" content="width=device-width">'
+    . '<script>function envPop(a){function b(c) {for (var d in a)c[d] = a[d];};'
+    . 'window.Env = Env = window.Env || {};b(Env);};'
+    . "!function(d){d.className=d.className.replace('no-js', '');}"
+    . "(document.documentElement);"
+    . 'envPop({"method":"' . $method . '"});</script><noscript>'
+    . $noscript . '</noscript>'
+    . '<title>' . $title . '</title>' . $head . '</head>'
+    . '<body' . $this->_bodyAttributes() . '>';
+
+    return $response;
+  }
+
+  /**
+   * get Body content
+   *
+   * @return mixed
+   */
+  public function renderBody()
+  {
+    return $this->getBody();
+  }
+
+  /**
+   * Attach an attribute to the body tag
+   *
+   * @param string $key   e.g. Class
+   * @param string $value e.g. fullpage
+   */
+  public function addBodyAttribute($key, $value)
+  {
+    $this->_bodyAttributes[$key] = $value;
+  }
+
+  /**
+   * @return string
+   */
+  protected function _bodyAttributes()
+  {
+    $attr = array();
+    foreach($this->_bodyAttributes as $k => $v)
+    {
+      $attr[] = " " . $k . '="' . HtmlElement::escape($v) . '"';
+    }
+    return implode("", $attr);
+  }
+
+  /**
+   * Closing Body and HTML Tags
+   *
+   * @return string
+   */
+  public function renderClosing()
+  {
+    return $this->getClosing() . '</body></html>';
+  }
+
+  /**
+   * Render whole webpage
+   *
+   * @return string
+   */
+  public function render()
+  {
+    return $this->renderHead() . $this->renderBody() . $this->renderClosing();
+  }
+
+  public function addRenderable(Renderable $render, $name = null)
+  {
+    if($name === null)
+    {
+      $this->_renderables[] = $render;
+    }
+    else
+    {
+      $this->_renderables[$name] = $render;
+    }
+    return $this;
+  }
+
+  public function removeRenderable($name)
+  {
+    unset($this->_renderables[$name]);
+    return $this;
+  }
+
+  public function renderables()
+  {
+    return $this->_renderables;
+  }
+}
---
+++ src/Cubex/View/Partial.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * @author  brooke.bryan
+ */
+namespace Cubex\View;
+
+use Cubex\Foundation\Renderable;
+
+class Partial implements Renderable
+{
+  /**
+   * @var bool
+   */
+  private $_escapeInput = true;
+  /**
+   * @var string
+   */
+  private $_template;
+  /**
+   * @var array|null
+   */
+  private $_variables;
+  /**
+   * @var
+   */
+  private $_elements;
+  /**
+   * @var array
+   */
+  private $_elementData = array();
+  /**
+   * @var string
+   */
+  protected $_glue = '';
+
+  /**
+   * @param string $template  (HTML Template)
+   * @param null   $variables (array of variables e.g. ["name","description"];
+   * @param bool   $escapeInput
+   */
+  public function __construct($template = '', $variables = null,
+                              $escapeInput = true)
+  {
+    $this->_escapeInput = $escapeInput;
+    $this->_template    = $template;
+    $this->_variables   = $variables === null ? array() : $variables;
+    $this->clearElements();
+  }
+
+  /**
+   * Switch escaping of input
+   *
+   * @param $escape
+   */
+  public function escapeInput($escape)
+  {
+    $this->_escapeInput = (bool)$escape;
+  }
+
+  /**
+   * Add element, args used in same order as defined in the constructor
+   */
+  public function addElement( /*$element,$element,...*/)
+  {
+    $this->_addItem(\func_get_args());
+
+    return $this;
+  }
+
+  /**
+   * @param $args
+   */
+  private function _addItem($args)
+  {
+    $element = $this->_template;
+    //Allow for changing the template at a later point in time, or handling in render
+    $this->_elementData[] = $args;
+
+    foreach($this->_variables as $arg => $key)
+    {
+      $element = \str_replace('{#' . $key . '}', $args[$arg], $element);
+    }
+    if(\is_array($args))
+    {
+      if($this->_escapeInput)
+      {
+        $this->_elements[] = \vsprintf(
+          $element,
+          \array_map(array('\Cubex\View\HtmlElement', 'escape'), $args)
+        );
+      }
+      else
+      {
+        $this->_elements[] = \vsprintf($element, $args);
+      }
+    }
+    else
+    {
+      if($this->_escapeInput)
+      {
+        $this->_elements[] = \sprintf($element, HtmlElement::escape($args));
+      }
+      else
+      {
+        $this->_elements[] = \sprintf($element, $args);
+      }
+    }
+  }
+
+  /**
+   * @param array $elements
+   *
+   * @return Partial
+   */
+  public function addElements(array $elements)
+  {
+    foreach($elements as $element)
+    {
+      $this->_addItem($element);
+    }
+
+    return $this;
+  }
+
+  /**
+   * @param string $glue
+   *
+   * @return Partial
+   */
+  public function setGlue($glue = '')
+  {
+    $this->_glue = $glue;
+
+    return $this;
+  }
+
+  /**
+   * @return string Rendered elements
+   */
+  public function render()
+  {
+    return \implode(
+      $this->_glue === null ? '' : $this->_glue, $this->_elements
+    );
+  }
+
+  /**
+   * @return string
+   */
+  public function __toString()
+  {
+    return $this->render();
+  }
+
+  /**
+   * Clear all elements added
+   */
+  public function clearElements()
+  {
+    $this->_elements = array();
+  }
+
+  /**
+   * @param $template
+   *
+   * @return Partial
+   */
+  public static function single($template /*$element,$element,...*/)
+  {
+    $partial = new Partial($template);
+    $args    = \func_get_args();
+    \array_shift($args);
+    $partial->_addItem($args);
+    return $partial;
+  }
+}
