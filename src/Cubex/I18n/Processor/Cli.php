2a76ab6b8e4f9e38d1c1928b052165a84c203b40
Brooke Bryan <brooke@justdevelop.it>
2013-01-14 17:47:20 +0000

    Translation tool (concept)

---
+++ src/Cubex/I18n/Processor/Cli.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @author  brooke.bryan
+ */
+
+namespace Cubex\I18n\Processor;
+
+use Cubex\Foundation\Config\ConfigTrait;
+use Cubex\Foundation\Config\Configurable;
+use Cubex\I18n\Translator\Reversulator;
+
+class Cli implements Configurable
+{
+  use ConfigTrait;
+
+  protected $_builder;
+
+  public function __construct()
+  {
+    $this->_builder = new Build(
+      $this->getConfig()->get("_cubex_")->getStr(
+        'project_base'
+      ), new Reversulator()
+    );
+
+    $this->_builder->addArea(
+      $this->getConfig()->get("project")->getStr('namespace', 'Project'), 2
+    );
+
+    if(PHP_WINDOWS_VERSION_MAJOR >= 6)
+    {
+      $this->setMsgfmt();
+    }
+
+    $this->addLanguage('it');
+    $this->addLanguage('de');
+
+    $this->run();
+  }
+
+  public function run()
+  {
+    $this->_builder->run();
+    return $this;
+  }
+
+  public function addLanguage($language)
+  {
+    $this->_builder->addLanguage($language);
+    return $this;
+  }
+
+  public function setMsgfmt(
+    $path = '"C:\Program Files (x86)\GnuWin32\bin\msgfmt.exe"'
+  )
+  {
+    $this->_builder->msgfmtPath($path);
+    return $this;
+  }
+}
