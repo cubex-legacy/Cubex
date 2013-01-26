fa6d75bb4ff5caa629ca0c5effb2d60e40db5549
Brooke Bryan <brooke@justdevelop.it>
2013-01-26 11:09:16 +0000

    config update

--- src/Cubex/I18n/Translator/googletranslator.php
+++ src/Cubex/I18n/Translator/googletranslator.php
@@ -28,42 +28,42 @@ class GoogleTranslator implements Translator
     $pattern = '/(?:%%|%(?:[0-9]+\$)?[+-]?(?:[ 0]|\'.)'
     . '?-?[0-9]*(?:\.[0-9]+)?[bcdeufFosxX])/';
     $replace = '<span class="notranslate">$0</span>';
-    $text    = \preg_replace($pattern, $replace, $text);
+    $text    = preg_replace($pattern, $replace, $text);
 
     $pattern = '/&[^\s]*;/';
     $replace = '<span class="notranslate">$0</span>';
-    $text    = \preg_replace($pattern, $replace, $text);
-    $data    = array(
-      'key'    => $this->getConfig()->get("locale")->getStr("google_api"),
+    $text    = preg_replace($pattern, $replace, $text);
+
+    $data = array(
+      'key'    => $this->config("i18n")->getStr("google_key"),
       'source' => \substr($sourceLanguage, 0, 2),
       'target' => \substr($targetLanguage, 0, 2),
       'q'      => $text
     );
 
-    $service = \curl_init();
-    \curl_setopt(
+    $service = curl_init();
+    curl_setopt(
       $service, CURLOPT_URL,
-      'https://www.googleapis.com/language/translate/v2?' . \http_build_query(
-        $data
-      )
+      'https://www.googleapis.com/language/translate/v2?' .
+      http_build_query($data)
     );
-    \curl_setopt($service, CURLOPT_RETURNTRANSFER, true);
-    \curl_setopt($service, CURLOPT_SSL_VERIFYHOST, false);
-    \curl_setopt($service, CURLOPT_SSL_VERIFYPEER, false);
-    $response = \curl_exec($service);
-    \curl_close($service);
+    curl_setopt($service, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($service, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($service, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($service);
+    curl_close($service);
 
     $pattern  = '/<span class="notranslate">([^<]*)<\/span>/';
-    $response = \preg_replace($pattern, '$1', \urldecode($response));
+    $response = preg_replace($pattern, '$1', urldecode($response));
 
-    $response = \json_decode($response);
+    $response = json_decode($response);
     if(isset($response->data->translations[0]->translatedText))
     {
       $translation = $response->data->translations[0]->translatedText;
     }
 
     $pattern     = '/<span class="notranslate">([^<]*)<\/span>/';
-    $translation = \preg_replace($pattern, '$1', $translation);
+    $translation = preg_replace($pattern, '$1', $translation);
 
     return $translation;
   }
