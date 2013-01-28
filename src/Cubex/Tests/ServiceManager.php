44543d3764bfc1edb7b90299da5015063c1728be
Gareth Evans <garoevans@gmail.com>
2013-01-28 19:35:26 +0000

    Updates to tests to use an extended service mangaer for testing purposes

---
+++ src/Cubex/Tests/ServiceManager.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author gareth.evans
+ */
+
+namespace Cubex\Tests;
+
+class ServiceManager extends \Cubex\ServiceManager\ServiceManager
+{
+  protected $_current = [];
+
+  public function tempBind($name, $obj)
+  {
+    if(isset($this->_shared[$name]))
+    {
+      $this->_current[$name] = $this->_shared[$name];
+    }
+
+    $this->_shared[$name] = $obj;
+  }
+
+  public function clearTemp($name)
+  {
+    if(isset($this->_shared[$name]))
+    {
+      unset($this->_shared[$name]);
+    }
+
+    if(isset($this->_current[$name]))
+    {
+      $this->_shared[$name] = $this->_current[$name];
+      unset($this->_current[$name]);
+    }
+  }
+
+  public function clearAllTemp()
+  {
+    foreach($this->_current as $name => $obj)
+    {
+      $this->clearTemp($name);
+    }
+  }
+}
