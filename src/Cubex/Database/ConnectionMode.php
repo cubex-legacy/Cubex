7037bac1c60c2c441811ef45f2d32b51da1523d1
Brooke Bryan <brooke@justdevelop.it>
2013-01-29 19:52:22 +0000

    connection mode helpers

--- src/Cubex/Database/ConnectionMode.php
+++ src/Cubex/Database/ConnectionMode.php
@@ -13,4 +13,14 @@ class ConnectionMode extends Enum
 
   const READ  = "r";
   const WRITE = "w";
+
+  public static function read()
+  {
+    return new static(static::READ);
+  }
+
+  public static function write()
+  {
+    return new static(static::WRITE);
+  }
 }
