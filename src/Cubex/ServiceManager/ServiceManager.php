76259e9d1f2dfd62ca137fc1a78462e5024d74fd
Brooke Bryan <brooke@justdevelop.it>
2013-01-30 10:19:46 +0000

    Service manager improvements

--- src/Cubex/ServiceManager/ServiceManager.php
+++ src/Cubex/ServiceManager/ServiceManager.php
@@ -16,6 +16,7 @@ class ServiceManager
 {
   protected $_services = array();
   protected $_shared = array();
+  protected $_alias = array();
 
   /**
    * @param $name
@@ -70,6 +71,12 @@ class ServiceManager
     return isset($this->_services[$name]);
   }
 
+  public function alias($alias, $serviceName)
+  {
+    $this->_alias[$alias] = $serviceName;
+    return $this;
+  }
+
   /**
    * @param               $name
    * @param ServiceConfig $config
@@ -93,21 +100,58 @@ class ServiceManager
     return $this;
   }
 
-  /**
-   * @param string $name
-   *
-   * @return $this
-   * @throws \InvalidArgumentException
-   */
-  public function unRegister($name)
+  public function bind($name, $class, $shared = true)
+  {
+    $config = new ServiceConfig();
+    $config->appendData(
+      [
+      'service_provider' => $class,
+      ]
+    );
+    $this->register($name, $config, $shared);
+    return $this;
+  }
+
+  public function bindInstance($name, $instance)
   {
     if(!$this->exists($name))
+    {
+      $this->_services[$name] = ['config' => null, 'shared' => true];
+    }
+    $this->_shared[$name] = $instance;
+    return $this;
+  }
+
+  public function setConfig($name, ServiceConfig $config)
+  {
+    if(isset($this->_services[$name]))
+    {
+      $this->_services[$name]['config'] = $config;
+    }
+    else
+    {
+      throw new \Exception("You cannot set a config on a non existent service");
+    }
+    return $this;
+  }
+
+  public function unRegister($name, $throw = true)
+  {
+    if($this->exists($name))
+    {
+      unset($this->_services[$name]);
+    }
+    else if($throw)
     {
       throw new \InvalidArgumentException("Service does not exist");
     }
 
-    unset($this->_services[$name]);
+    return $this;
+  }
 
+  public function unbind($name, $throw = true)
+  {
+    $this->unRegister($name, $throw);
     return $this;
   }
 
@@ -130,10 +174,9 @@ class ServiceManager
         $provider = $config->getStr("service_provider");
         if($provider)
         {
-          $factoryClass = $provider;
           if(class_exists($provider))
           {
-            $service = new $provider();
+            $service = $this->_buildClass($provider, $name);
           }
         }
         else
@@ -148,15 +191,8 @@ class ServiceManager
           {
             throw new \Exception("Invalid service factory");
           }
-        }
 
-        if($service instanceof Service)
-        {
           $service->configure($config);
-        }
-        else
-        {
-          throw new \Exception("Invalid service created by '$factoryClass'");
         }
 
         if($service instanceof ServiceManagerAware)
@@ -166,7 +202,8 @@ class ServiceManager
 
         if($this->_services[$name]['shared'])
         {
-          return $this->_shared[$name] = $service;
+          $this->bindInstance($name, $service);
+          return $this->get($name);
         }
         else
         {
@@ -182,6 +219,68 @@ class ServiceManager
     {
       throw new \InvalidArgumentException("Service does not exist");
     }
+  }
+
+  protected function _attemptClass($class)
+  {
+    if(isset($this->_alias[$class]))
+    {
+      return $this->_create($this->_alias[$class]);
+    }
+    else
+    {
+      return $this->_buildClass($class);
+    }
+  }
+
+  protected function _buildClass($class, $name = null)
+  {
+    $reflection = new \ReflectionClass($class);
+
+    if(!$reflection->isInstantiable())
+    {
+      $message = "Target [$class] is not instantiable.";
+      throw new \RuntimeException($message);
+    }
+
+    $constructor = $reflection->getConstructor();
+    if($constructor === null)
+    {
+      return new $class;
+    }
+
+    return $reflection->newInstanceArgs(
+      $this->_getConstructorArgs($constructor->getParameters(), $name)
+    );
+  }
+
+  protected function _getConstructorArgs(array $params, $name = null)
+  {
+    $args = [];
+    foreach($params as $param)
+    {
+      /** @var $param \ReflectionParameter */
+      $class = $param->getClass();
+      if($class === null)
+      {
+        if(!$param->isDefaultValueAvailable())
+        {
+          throw new \RuntimeException(
+            "Unable to resolve dependency on $name [$param]"
+          );
+        }
+        else
+        {
+          $args[] = $param->getDefaultValue();
+        }
+      }
+      else
+      {
+        $args[] = $this->_attemptClass($class->name);
+      }
+    }
+
+    return $args;
   }
 
   /**
