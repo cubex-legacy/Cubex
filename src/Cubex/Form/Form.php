9bca001552d70c697b565ef71760a99eed0e2cc7
Brooke Bryan <brooke@justdevelop.it>
2013-02-01 20:07:52 +0000

    public attribute exists

--- src/Cubex/Form/Form.php
+++ src/Cubex/Form/Form.php
@@ -60,6 +60,11 @@ class Form extends DataMapper implements Renderable
     return $this;
   }
 
+  public function attributeExists($attribute)
+  {
+    return $this->_attributeExists($attribute);
+  }
+
   public function setFormName($name)
   {
     $this->_formName                  = $name;
