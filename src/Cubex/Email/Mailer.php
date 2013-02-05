a83c2ca70e711d29442f710a56b3829475e6c6a0
Gareth Evans <garoevans@gmail.com>
2013-02-05 16:58:26 +0000

    Added a mailer class that accepts a mailer service - Allows us to add mailer specific functionality without affecting the   services

---
+++ src/Cubex/Email/Mailer.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * @author gareth.evans
+ */
+
+namespace Cubex\Email;
+
+use Cubex\ServiceManager\ServiceConfig;
+
+class Mailer
+{
+  protected $_service;
+
+  public function __construct(EmailService $service)
+  {
+    $this->_service = $service;
+  }
+
+  /**
+   * @param ServiceConfig $config
+   *
+   * @return $this
+   */
+  public function configure(ServiceConfig $config)
+  {
+    $this->_service->configure($config);
+
+    return $this;
+  }
+
+  /**
+   * @param $subject
+   *
+   * @return $this
+   */
+  public function setSubject($subject)
+  {
+    $this->_service->setSubject($subject);
+
+    return $this;
+  }
+
+  /**
+   * @param $body
+   *
+   * @return $this
+   */
+  public function setBody($body)
+  {
+    $this->_service->setBody($body);
+
+    return $this;
+  }
+
+  /**
+   * @param bool $bool
+   *
+   * @return $this
+   */
+  public function isHtml($bool = true)
+  {
+    $this->_service->isHtml($bool);
+
+    return $this;
+  }
+
+  /**
+   * @param      $email
+   * @param null $name
+   *
+   * @return $this
+   */
+  public function setSender($email, $name = null)
+  {
+    $this->_service->setSender($email, $name);
+
+    return $this;
+  }
+
+  /**
+   * @param      $email
+   * @param null $name
+   *
+   * @return $this
+   */
+  public function addRecipient($email, $name = null)
+  {
+    $this->_service->addRecipient($email, $name);
+
+    return $this;
+  }
+
+  /**
+   * @param      $email
+   * @param null $name
+   *
+   * @return $this
+   */
+  public function addCC($email, $name = null)
+  {
+    $this->_service->addCC($email, $name);
+
+    return $this;
+  }
+
+  /**
+   * @param      $email
+   * @param null $name
+   *
+   * @return $this
+   */
+  public function addBCC($email, $name = null)
+  {
+    $this->_service->addBCC($email, $name);
+
+    return $this;
+  }
+
+  /**
+   * @param $name
+   * @param $value
+   *
+   * @return $this
+   */
+  public function addHeader($name, $value)
+  {
+    $this->_service->addHeader($name, $value);
+
+    return $this;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function send()
+  {
+    return $this->_service->send();
+  }
+}
