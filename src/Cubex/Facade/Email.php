dbe3271e02276e4bca0e78911f91c29e8c7ddd45
Gareth Evans <garoevans@gmail.com>
2013-02-08 15:46:12 +0000

    Added a facade to deal with the email service and a shorthand way of sending mail

---
+++ src/Cubex/Facade/Email.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @author  gareth.evans
+ */
+namespace Cubex\Facade;
+
+class Email extends BaseFacade
+{
+  /**
+   * @return \Cubex\Email\EmailService
+   */
+  public static function getAccessor()
+  {
+    return static::getServiceManager()->get("email");
+  }
+
+  /**
+   * @param string|array $to      If $to is an array it will use index 0 as the
+   *                              email and index 1 as the name.
+   * @param string       $subject
+   * @param string       $message
+   * @param array        $headers Headers should be an array keyed by the header
+   *                              name and filled with the header value.
+   *
+   * @return mixed
+   */
+  public function mail($to, $subject, $message, $headers = array())
+  {
+    static::getAccessor()->reset();
+
+    if(is_array($to))
+    {
+      static::getAccessor()->addRecipient($to[0], $to[1]);
+    }
+    else
+    {
+      static::getAccessor()->addRecipient($to);
+    }
+
+    static::getAccessor()->setSubject($subject);
+    static::getAccessor()->setBody($message);
+
+    foreach($headers as $headerName => $headerValue)
+    {
+      static::getAccessor()->addHeader($headerName, $headerValue);
+    }
+
+    return static::getAccessor()->send();
+  }
+}
