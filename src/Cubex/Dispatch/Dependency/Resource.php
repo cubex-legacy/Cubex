dda00f007ff1c973eab304d4f699b313a811f1f0
Gareth Evans <garoevans@gmail.com>
2013-02-22 15:40:37 +0000

    Stop dispatch loading duplicate external resources

--- src/Cubex/Dispatch/Dependency/Resource.php
+++ src/Cubex/Dispatch/Dependency/Resource.php
@@ -21,6 +21,12 @@ class Resource extends Dependency
     "packages" => []
   );
 
+  protected $_priorotisedProtocols = array(
+    "https://",
+    "//",
+    "http://"
+  );
+
   /**
    * @param Resource\TypeEnum $type
    *
@@ -40,6 +46,87 @@ class Resource extends Dependency
     }
 
     return $resourceUris;
+  }
+
+  /**
+   * Merges and duplicate URIs preserving the highest priority protocol
+   *
+   * @param Resource\TypeEnum $type
+   */
+  public function mergeDuplicateExternalUris(TypeEnum $type)
+  {
+    $resourceUris = ipull(self::$_requires[(string)$type], "uri");
+    $externalUris = array_filter($resourceUris, [$this, "isExternalUri"]);
+
+    $externalUriByProtocol = $this->mapUrisByPriorotisedProtocol($externalUris);
+
+    $mergedUri = [];
+
+    foreach($this->_priorotisedProtocols as $protocol)
+    {
+      foreach($externalUriByProtocol[$protocol] as $strippedUri => $originalKey)
+      {
+        if(array_key_exists($strippedUri, $mergedUri))
+        {
+          unset(self::$_requires[(string)$type][$originalKey]);
+        }
+        else
+        {
+          $mergedUri[$strippedUri] = $strippedUri;
+        }
+      }
+    }
+  }
+
+  /**
+   * Build an array keyed by the priorotised protocols containing the an array
+   * of a relevant uri (protocol stripper) as key and the original key as the
+   * value;
+   *
+   * In:
+   * Array
+   * (
+   *   [0] => http://twitter.github.com/bootstrap/assets/css/bootstrap.css
+   *   [1] => //twitter.github.com/bootstrap/assets/css/bootstrap.css
+   * )
+   *
+   * Out:
+   * Array
+   * (
+   *   [https://] => Array
+   *   (
+   *   )
+   *   [//] => Array
+   *   (
+   *     [twitter.github.com/bootstrap/assets/css/bootstrap.css] => 1
+   *   )
+   *   [http://] => Array
+   *   (
+   *     [twitter.github.com/bootstrap/assets/css/bootstrap.css] => 0
+   *   )
+   * )
+   *
+   * @param array $uris
+   *
+   * @return array
+   */
+  public function mapUrisByPriorotisedProtocol(array $uris)
+  {
+    $externalUriByProtocol = array_fill_keys($this->_priorotisedProtocols, []);
+
+    foreach($uris as $originalKey => $externalUri)
+    {
+      foreach($this->_priorotisedProtocols as $protocol)
+      {
+        if(strpos($externalUri, $protocol) === 0)
+        {
+          $strippedUri = substr($externalUri, strlen($protocol));
+          $externalUriByProtocol[$protocol][$strippedUri] = $originalKey;
+        }
+      }
+    }
+
+    return $externalUriByProtocol;
   }
 
   /**
@@ -73,6 +160,7 @@ class Resource extends Dependency
 
     $event->setFile($file);
     $this->_requireResource($event);
+    $this->mergeDuplicateExternalUris($type);
   }
 
   /**
