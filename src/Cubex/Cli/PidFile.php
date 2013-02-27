6c795823343cfc7d3bdb30c4ff6c936e353c9e6e
richard.gooding <richard.gooding@justdevelop.it>
2013-02-27 13:40:48 +0000

    Added support for PID files in CLI applications

---
+++ src/Cubex/Cli/PidFile.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @author  Richard.Gooding
+ */
+
+namespace Cubex\Cli;
+
+use Cubex\Container\Container;
+use Cubex\Exception\CubexException;
+
+class PidFile
+{
+  private $_pidFilePath;
+  private $_enabled;
+
+  public function __construct($path = "")
+  {
+    $this->_enabled = true;
+
+    // no PID file on Windows
+    /*if(strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
+    {
+      $this->_enabled = false;
+    }*/
+
+    $conf = Container::config()->get('pidfile');
+    if($conf)
+    {
+      $this->_enabled = $conf->getBool('enabled', true);
+    }
+
+    if($this->_enabled)
+    {
+      $this->_pidFilePath = $this->_getPidFilePath($path);
+      $this->_createPidFile();
+    }
+  }
+
+  public function __destruct()
+  {
+    $this->_deletePidFile();
+  }
+
+  private function _getPidFilePath($path = "")
+  {
+    if($path == "")
+    {
+      $conf = Container::config()->get('pidfile');
+      if($conf) $path = $conf->getStr('path', "");
+    }
+    if($path == "")
+    {
+      if(strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
+      {
+        $path = realpath(dirname(WEB_ROOT)) . DS . $_REQUEST['__path__'] . '.pid';
+      }
+      else
+      {
+        $path = '/var/run/' . $_REQUEST['__path__'] . '.pid';
+      }
+    }
+    return $path;
+  }
+
+  private function _createPidFile()
+  {
+    if(! $this->_enabled) return;
+
+    if(file_exists($this->_pidFilePath))
+    {
+      $oldpid = trim(file_get_contents($this->_pidFilePath));
+      if(file_exists('/proc/' . $oldpid))
+      {
+        $cmdLine = explode(chr(0), file_get_contents('/proc/' . $oldpid . '/cmdline'));
+        if(in_array($_REQUEST['__path__'], $cmdLine))
+        {
+          throw new \Exception('Another instance is already running, PID ' . $oldpid);
+        }
+      }
+      unlink($this->_pidFilePath);
+    }
+
+    file_put_contents($this->_pidFilePath, getmypid());
+    if(! file_exists($this->_pidFilePath))
+    {
+      throw new \Exception('Failed to create PID file');
+    }
+  }
+
+  private function _deletePidFile()
+  {
+    if($this->_enabled && file_exists($this->_pidFilePath)) unlink($this->_pidFilePath);
+  }
+}
