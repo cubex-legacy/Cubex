21834bd6aad35c00d58cd27d6165d210a7b26d01
Brooke Bryan <brooke@justdevelop.it>
2013-03-23 17:34:38 +0000

    curl request

---
+++ src/Cubex/Helpers/Curl.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author  brooke.bryan
+ */
+
+namespace Cubex\Helpers;
+
+use Cubex\Exception\CubexException;
+
+class Curl
+{
+  public static function request($uri, $timeout = 30)
+  {
+    $res = curl_init();
+    curl_setopt($res, CURLOPT_URL, $uri);
+    curl_setopt($res, CURLOPT_HEADER, 0);
+    curl_setopt($res, CURLOPT_CONNECTTIMEOUT, $timeout);
+    curl_setopt($res, CURLOPT_TIMEOUT, $timeout);
+    curl_setopt($res, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($res);
+    $errno    = curl_errno($res);
+    $error    = curl_error($res);
+    if((!is_string($response) || !strlen($response)))
+    {
+      throw new CubexException(
+        "Failed to connect to " . $uri, $errno, $error
+      );
+    }
+    curl_close($res);
+    return $response;
+  }
+}
