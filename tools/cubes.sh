3b93e82e5c7cd1d4b8c28e1106f0ae1046077580
Brooke Bryan <brooke@justdevelop.it>
2013-03-27 16:58:28 +0000

    Bin subtree split

--- tools/cubes.sh
+++ tools/cubes.sh
@@ -1,3 +1,9 @@
-git subsplit init git@github.com:qbex/Cubex
-git subsplit publish src/Cubex/Foundation:git@github.com:qbex/Foundation.git
+echo "Updating Bin Repo"
+git subsplit init git@github.com:qbex/project
+git subsplit publish bin:git@github.com:qbex/bin.git
 rm -rf .subsplit/
+
+#echo "Updating Foundation"
+#git subsplit init git@github.com:qbex/Cubex
+#git subsplit publish src/Cubex/Foundation:git@github.com:qbex/Foundation.git
+#rm -rf .subsplit/
