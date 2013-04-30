4001543b9e13480870f1015f9d0e13e675967233
richard.gooding <richard.gooding@justdevelop.it>
2013-04-30 15:41:56 +0100

    Added ExpiringEphemeralCache to store data in memory with a timeout

---
+++ src/Cubex/Data/Ephemeral/ExpiringEphemeralCache.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @author  richard.gooding
+ */
+
+namespace Cubex\Data\Ephemeral;
+
+class ExpiringCacheObject
+{
+  public $data;
+  public $expires;
+  public function expired()
+  {
+    return ($this->expires != 0) && ($this->expires <= microtime(true));
+  }
+}
+
+class ExpiringEphemeralCache extends EphemeralCache
+{
+  const CACHE_PREFIX = '__EXPIRING__';
+
+  public static function generateID($id)
+  {
+    if(strpos($id, self::CACHE_PREFIX) === 0)
+    {
+      return $id;
+    }
+    else
+    {
+      return self::CACHE_PREFIX . parent::generateID($id);
+    }
+  }
+
+  public static function storeCache($id, $data, $source, $lifetime = 0)
+  {
+    $dataObj = new ExpiringCacheObject();
+    $dataObj->data = $data;
+    $dataObj->expires = $lifetime ? microtime(true) + $lifetime : 0;
+    parent::storeCache($id, $dataObj, $source);
+  }
+
+  public static function inCache($id, $source)
+  {
+    $exists = false;
+    $id = static::generateID($id);
+    if(isset(static::$_storage[static::_storageKey($source)][$id]))
+    {
+      $data = static::$_storage[static::_storageKey($source)][$id];
+      if(($data instanceof ExpiringCacheObject) && (! $data->expired()))
+      {
+        $exists = true;
+      }
+    }
+    return $exists;
+  }
+
+  public static function getCache($id, $source, $default = null)
+  {
+    $data = parent::getCache($id, $source, $default);
+    if($data instanceof ExpiringCacheObject)
+    {
+      return $data->data;
+    }
+    else
+    {
+      return $data;
+    }
+  }
+}
