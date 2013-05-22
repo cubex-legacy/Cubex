d9c6a56fd6c97b6a835dc77cd524c368b393a897
Brooke Bryan <brooke@justdevelop.it>
2013-05-22 11:25:35 +0100

    New subsplits

--- tools/cubes.sh
+++ tools/cubes.sh
@@ -1,9 +1,14 @@
-echo "Updating Bin Repo"
+rm -rf .subsplit/
+
+
+
+git subsplit init git@github.com:qbex/Cubex
+git subsplit publish src/Cubex/Type:git@github.com:qbex/type.git
+git subsplit publish src/Cubex/Data:git@github.com:qbex/data.git
+rm -rf .subsplit/
+
+
+
 git subsplit init git@github.com:qbex/project
 git subsplit publish bin:git@github.com:qbex/bin.git
 rm -rf .subsplit/
-
-#echo "Updating Foundation"
-#git subsplit init git@github.com:qbex/Cubex
-#git subsplit publish src/Cubex/Foundation:git@github.com:qbex/Foundation.git
-#rm -rf .subsplit/
