0afc7eb5f85cdcf809b3668bc353a1cd968e0a62
richard.gooding <richard.gooding@justdevelop.it>
2013-08-19 18:02:39 +0100

    Added batched callable queue consumer

---
+++ src/Cubex/Queue/BatchCallableQueueConsumer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author  Richard.Gooding
+ */
+
+namespace Cubex\Queue;
+
+class BatchCallableQueueConsumer extends CallableQueueConsumer
+  implements IBatchQueueConsumer
+{
+  protected $_batchSize = 10;
+
+  public function runBatch()
+  {
+    return [];
+  }
+
+  public function setBatchSize($batchSize)
+  {
+    $this->_batchSize = $batchSize;
+  }
+
+  public function getBatchSize()
+  {
+    return $this->_batchSize;
+  }
+
+  public function process(IQueue $queue, $data, $taskId = null)
+  {
+    parent::process($queue, $data);
+  }
+}
