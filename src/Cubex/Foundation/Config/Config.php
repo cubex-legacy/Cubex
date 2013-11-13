0af0da530e5566214502ff3bd6e6c6a42f3e8e36
Brooke Bryan <brooke@justdevelop.it>
2013-11-13 15:33:58 +0000

    Configs support class defined constant replacements (Fully Qualified Only)

--- src/Cubex/Foundation/Config/Config.php
+++ src/Cubex/Foundation/Config/Config.php
@@ -10,5 +10,41 @@ use Cubex\Data\Handler\HandlerTrait;
 
 class Config implements \IteratorAggregate, IDataHandler, \ArrayAccess
 {
-  use HandlerTrait;
+  use HandlerTrait
+  {
+    setData as protected traitSetData;
+  }
+
+  /**
+   * @param $name
+   * @param $value
+   *
+   * @return $this
+   */
+  public function setData($name, $value)
+  {
+    if(is_scalar($value) && strstr($value, "::") !== false)
+    {
+      if(defined($value))
+      {
+        $value = constant($value);
+      }
+    }
+    $this->traitSetData($name, $value);
+    return $this;
+  }
+
+  /**
+   * @param $data
+   *
+   * @return $this
+   */
+  public function hydrate($data)
+  {
+    foreach($data as $name => $value)
+    {
+      $this->setData($name, $value);
+    }
+    return $this;
+  }
 }
