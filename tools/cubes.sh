2a87cca4e121bd3065b84c4e574ab9c99d9570a7
Brooke Bryan <brooke@justdevelop.it>
2013-11-20 18:44:53 +0000

    No longer pull the qbex/bin from qbex/project

--- tools/cubes.sh
+++ tools/cubes.sh
@@ -1,14 +1,6 @@
 rm -rf .subsplit/
 
-
-
 git subsplit init git@github.com:qbex/Cubex
 git subsplit publish src/Cubex/Type:git@github.com:qbex/type.git
 git subsplit publish src/Cubex/Data:git@github.com:qbex/data.git
-rm -rf .subsplit/
-
-
-
-git subsplit init git@github.com:qbex/project
-git subsplit publish bin:git@github.com:qbex/bin.git
 rm -rf .subsplit/
