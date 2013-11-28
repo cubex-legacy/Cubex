abd15decf8bc848a5a88a3b93b1fe84a35da4a9d
richard.gooding <richard.gooding@justdevelop.it>
2013-11-28 16:31:04 +0000

    Added DatabaseMailer email service that writes messages to a database table instead of sending them

---
+++ src/Cubex/Email/Service/DatabaseMailer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author  Richard.Gooding
+ */
+
+namespace Cubex\Email\Service;
+
+use Cubex\Email\Service\DatabaseMailer\MailerMapper;
+
+/**
+ * Email service that just writes the messages into a database table instead
+ * of sending them
+ */
+class DatabaseMailer extends Mail
+{
+  protected function _newMapper($id = null)
+  {
+    $mapper = new MailerMapper($id);
+    $mapper->setTableName($this->config()->getStr('tablename', 'dbmailer'));
+    $mapper->setServiceName($this->config()->getStr('dbservice', 'db'));
+    return $mapper;
+  }
+
+  public function send()
+  {
+    $rawMessage = $this->_generateMessageAndSetHeaders(true);
+
+    $mapper = $this->_newMapper();
+    $mapper->recipients = implode(",", $this->_recipients);
+    $mapper->ccs = implode(",", $this->_ccs);
+    $mapper->bccs = implode(",", $this->_bccs);
+    $mapper->subject = $this->_subject;
+    $mapper->htmlBody = $this->_htmlBody;
+    $mapper->textBody = $this->_textBody;
+    $mapper->from = implode(",", $this->_from);
+    $mapper->sender = $this->_sender;
+    $mapper->returnPath = $this->_returnPath;
+    $mapper->headers = implode("\r\n", $this->_headers);
+    $mapper->files = implode("\r\n", $this->_files);
+    $mapper->rawMessage = $rawMessage;
+    $mapper->saveChanges();
+  }
+}
---
+++ src/Cubex/Email/Service/DatabaseMailer/MailerMapper.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @author  Richard.Gooding
+ */
+
+namespace Cubex\Email\Service\DatabaseMailer;
+
+use Cubex\Mapper\Database\RecordMapper;
+
+/**
+ * Mapper for messages stored in the database by the DatabaseMailer service
+ */
+class MailerMapper extends RecordMapper
+{
+  /**
+   * @datatype varchar(255)
+   */
+  public $recipients;
+  /**
+   * @datatype varchar(255)
+   */
+  public $ccs;
+  /**
+   * @datatype varchar(255)
+   */
+  public $bccs;
+  /**
+   * @datatype varchar(255)
+   */
+  public $subject;
+  /**
+   * @datatype mediumtext
+   */
+  public $htmlBody;
+  /**
+   * @datatype mediumtext
+   */
+  public $textBody;
+  /**
+   * @datatype varchar(255)
+   */
+  public $from;
+  /**
+   * @datatype varchar(255)
+   */
+  public $sender;
+  /**
+   * @datatype varchar(255)
+   */
+  public $returnPath;
+  /**
+   * @datatype text
+   */
+  public $headers;
+  /**
+   * @datatype varchar(255)
+   */
+  public $files;
+  /**
+   * @datatype mediumtext
+   */
+  public $rawMessage;
+}
