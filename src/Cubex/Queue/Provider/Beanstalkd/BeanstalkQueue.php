a9ef62a849c7941b6e463fbeb70fd494de66d155
Brooke Bryan <brooke@justdevelop.it>
2013-11-28 18:17:42 +0000

    Batch support within Beanstalkd queue provider

--- src/Cubex/Queue/Provider/Beanstalkd/BeanstalkQueue.php
+++ src/Cubex/Queue/Provider/Beanstalkd/BeanstalkQueue.php
@@ -5,6 +5,7 @@
 
 namespace Cubex\Queue\Provider\Beanstalkd;
 
+use Cubex\Queue\IBatchQueueConsumer;
 use Cubex\Queue\IQueue;
 use Cubex\Queue\IQueueConsumer;
 use Cubex\Queue\IQueueProvider;
@@ -49,30 +50,26 @@ class BeanstalkQueue implements IQueueProvider
     try
     {
       $this->_waits = 0;
-      $conn         = $this->_connection();
       while(true)
       {
-        $job = $conn->reserveFromTube($queue->name());
-        if($job !== false)
+        if($consumer instanceof IBatchQueueConsumer)
         {
-          $result = $consumer->process($queue, $job->getData());
-          if($result)
-          {
-            $conn->delete($job);
-          }
+          $this->_consumeBatch($queue, $consumer);
         }
         else
         {
-          $waitTime = $consumer->waitTime($this->_waits);
-          if($waitTime === false)
-          {
-            return false;
-          }
-          else if($waitTime > 0)
-          {
-            $this->_waits++;
-            sleep($waitTime);
-          }
+          $this->_consumeSingle($queue, $consumer);
+        }
+
+        $waitTime = $consumer->waitTime($this->_waits);
+        if($waitTime === false)
+        {
+          return false;
+        }
+        else if($waitTime > 0)
+        {
+          $this->_waits++;
+          sleep($waitTime);
         }
       }
     }
@@ -82,5 +79,64 @@ class BeanstalkQueue implements IQueueProvider
     }
     $consumer->shutdown();
     return true;
+  }
+
+  protected function _consumeSingle(IQueue $queue, IQueueConsumer $consumer)
+  {
+    $conn = $this->_connection();
+    while(true)
+    {
+      $job = $conn->reserveFromTube($queue->name());
+      if($job === false)
+      {
+        break;
+      }
+      $result = $consumer->process($queue, $job->getData());
+      if($result)
+      {
+        $conn->delete($job);
+      }
+      else
+      {
+        $conn->release($job);
+      }
+    }
+  }
+
+  protected function _consumeBatch(IQueue $queue, IBatchQueueConsumer $consumer)
+  {
+    $conn = $this->_connection();
+    while(true)
+    {
+      $jobs = $results = [];
+      for($i = 0; $i < $consumer->getBatchSize(); $i++)
+      {
+        $job                 = $conn->reserveFromTube($queue->name());
+        $jobs[$job->getId()] = $job;
+        $consumer->process($queue, $job->getData(), $job->getId());
+        if($job === false)
+        {
+          break;
+        }
+      }
+
+      $results = $consumer->runBatch();
+      if(empty($results))
+      {
+        break;
+      }
+
+      foreach($results as $jobId => $result)
+      {
+        if($result)
+        {
+          $conn->delete($jobs[$jobId]);
+        }
+        else
+        {
+          $conn->release($jobs[$jobId]);
+        }
+      }
+    }
   }
 }
