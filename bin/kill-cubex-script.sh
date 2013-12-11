e5b1e3edea7bf61abc951e5a547d6b6e876ef8a3
Tom Kay <tom.kay@justdevelop.it>
2013-12-11 17:34:55 +0000

    fix kill script to reference correct bin location and optionally accept instance name

--- bin/kill-cubex-script.sh
+++ bin/kill-cubex-script.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
 then
-        echo "Usage: $0 className"
-        exit 1
+  echo "Usage: $0 className"
+  exit 1
 fi
 
 CLASSNAME="$1"
+INST="$2"
 
-PID=`ps ax | grep "bin/cubex" | grep "$CLASSNAME" | grep -v grep | awk '{print $1}' | xargs echo`
+if [ "$INST" != "" ]
+then
+  INST="$INST(\s|$)"
+fi
+
+PID=`ps ax | grep "/cubex" | grep "$CLASSNAME" | grep -E "$INST" | grep -v grep | awk '{print $1}'`
 
 if [ "$PID" = "" ]
 then
-        echo "not running"
+  echo "not running"
 else
   for P in $PID
   do
-          echo "Killing Cubex Class $CLASSNAME PID $P"
-          kill $P
-        done
+    echo "Killing Cubex Class $CLASSNAME PID $P"
+    kill $P
+  done
 fi
