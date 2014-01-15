2b1eec46fa7dbd82d983b31c2ab9ad18675b0c43
Jay Francis <jay.francis@justdevelop.it>
2014-01-15 12:52:00 +0000

    Added gt-ie8 to <html> class attribute

--- src/Cubex/Core/Response/Webpage.php
+++ src/Cubex/Core/Response/Webpage.php
@@ -338,7 +338,7 @@ class Webpage implements
     . '<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->'
     . '<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->'
     . '<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->'
-    . '<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->'
+    . '<!--[if gt IE 8]><!--><html class="no-js gt-ie8"><!--<![endif]-->'
     . "\n" . '<head><meta charset="' . $charset . '" />'
     . '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">'
     . '<meta name="viewport" content="width=device-width,initial-scale=1.0">'
