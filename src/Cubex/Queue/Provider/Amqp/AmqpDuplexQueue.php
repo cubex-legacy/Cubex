7d971ded404d2cb601be6c0a15e5727501f721d3
richard.gooding <richard.gooding@justdevelop.it>
2014-02-28 12:46:15 +0000

    Added description of class

--- src/Cubex/Queue/Provider/Amqp/AmqpDuplexQueue.php
+++ src/Cubex/Queue/Provider/Amqp/AmqpDuplexQueue.php
@@ -10,6 +10,11 @@ use Cubex\Queue\IQueue;
 use Cubex\Queue\IQueueConsumer;
 use Cubex\ServiceManager\ServiceConfigTrait;
 
+/**
+ * AMQP queue provider that uses separate connections for consuming and pushing.
+ * This is especially useful when using RabbitMQ so that the rate-limiting of
+ * push messages doesn't affect consumers.
+ */
 class AmqpDuplexQueue implements IBatchQueueProvider
 {
   use ServiceConfigTrait;
