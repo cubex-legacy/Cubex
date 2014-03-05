2e2ac979258fe22d6ed0bc8f896f658c2eaf353b
richard.gooding <richard.gooding@justdevelop.it>
2014-03-05 19:24:41 +0000

    Added AmqpQueue provider that wraps the alternative AMQP implementations

---
+++ src/Cubex/Queue/Provider/Amqp/AmqpQueue.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @author  richard.gooding
+ */
+
+namespace Cubex\Queue\Provider\Amqp;
+
+use Cubex\Queue\IBatchQueueProvider;
+use Cubex\Queue\IQueue;
+use Cubex\Queue\IQueueConsumer;
+use Cubex\ServiceManager\ServiceConfig;
+use Cubex\ServiceManager\ServiceConfigTrait;
+
+/**
+ * AMQP queue provider. Tries to use the PECL amqp module (AmqpQueueExtension)
+ * if available then falls back to the php-amqplib library (AmqpQueueLibrary)
+ */
+class AmqpQueue implements IBatchQueueProvider
+{
+  /**
+   * @var AmqpQueueExtension|AmqpQueueLibrary
+   */
+  private $_queueProvider;
+
+  public function __construct()
+  {
+    $this->_queueProvider = extension_loaded('amqp') ?
+      new AmqpQueueExtension() : new AmqpQueueLibrary();
+  }
+
+  public function configure(ServiceConfig $config)
+  {
+    $this->_queueProvider->configure($config);
+  }
+
+  public function push(IQueue $queue, $data = null, $delay = 0)
+  {
+    $this->_queueProvider->push($queue, $data, $delay);
+  }
+
+  public function pushBatch(IQueue $queue, array $data, $delay = 0)
+  {
+    $this->_queueProvider->pushBatch($queue, $data, $delay);
+  }
+
+  public function consume(IQueue $queue, IQueueConsumer $consumer)
+  {
+    $this->_queueProvider->consume($queue, $consumer);
+  }
+
+  public function disconnect()
+  {
+    $this->_queueProvider->disconnect();
+  }
+}
