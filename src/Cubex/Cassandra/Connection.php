5dcd4009443ad075eedb0c904256d8aca4ada964
richard.gooding <richard.gooding@justdevelop.it>
2014-10-08 13:55:45 +0100

    Changed default cassandra consistency level from QUORUM to LOCAL_QUORUM

--- src/Cubex/Cassandra/Connection.php
+++ src/Cubex/Cassandra/Connection.php
@@ -276,7 +276,7 @@ class Connection
   }
 
   public function flushBatch(
-    $atomic = true, $writeConsistencyLevel = ConsistencyLevel::QUORUM
+    $atomic = true, $writeConsistencyLevel = ConsistencyLevel::LOCAL_QUORUM
   )
   {
     if($this->_batchMutation === null || empty($this->_batchMutation))
@@ -310,7 +310,7 @@ class Connection
   }
 
   public function closeBatch(
-    $atomic = true, $writeConsistencyLevel = ConsistencyLevel::QUORUM
+    $atomic = true, $writeConsistencyLevel = ConsistencyLevel::LOCAL_QUORUM
   )
   {
     $this->flushBatch($atomic, $writeConsistencyLevel);
